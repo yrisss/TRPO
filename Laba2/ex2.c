8883c54c122450e8f16ff7b19abc1cf399f587d0
yrisss <114473485+yrisss@users.noreply.github.com>
2022-12-12 22:25:19 +0300

    Update ex2.c

--- Laba2/ex2.c
+++ Laba2/ex2.c
@@ -6,29 +6,29 @@ int main() {
 	setlocale (LC_ALL, "");
 	int x;
 	
-	printf("Введите возраст от 20 до 69: ");
+	printf("Р’РІРµРґРёС‚Рµ РІРѕР·СЂР°СЃС‚ РѕС‚ 20 РґРѕ 69: ");
 	
 	scanf("%d", &x);
 	
 	if((x<20) || (x>69)) 
-	printf("Ошибка! Указан неверный возраст.");
+	printf("РћС€РёР±РєР°! РЈРєР°Р·Р°РЅ РЅРµРІРµСЂРЅС‹Р№ РІРѕР·СЂР°СЃС‚.");
 
 	else {
 		switch(x%10) {
 		
 		
-		case 1: printf("%d год", x); break;
+		case 1: printf("%d РіРѕРґ", x); break;
 		
 		case 2:
 		case 3:
-		case 4: printf("%d года", x); break;
+		case 4: printf("%d РіРѕРґР°", x); break;
 		
 		case 5:
 		case 6:
 		case 7:
 		case 8:
 		case 9:
-		case 0: printf("%d лет", x); break;
+		case 0: printf("%d Р»РµС‚", x); break;
 	}
 		 
 	}
